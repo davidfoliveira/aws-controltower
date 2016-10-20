1370ee8a76eb031ff8e2ea5585030988dec3c17b
David Oliveira <david.oliveira@skyscanner.net>
2016-10-20 14:55:48 +0100

    aws.autoscaling.scale() is now working

---
+++ test/aws/01_1_autoscaling.js
@@ -0,0 +1,18 @@
+var
+    autoscaling = require('../../lib/aws/autoscaling');
+
+autoscaling.scale('eu-west-1', ['JacquardPreProd-JacquardDirectEuWest1AutoScalingGroup-193UO71ZHICH2'], '+20%', {dryRun: true}, function(err, res) {
+	console.log(err);
+	console.log(res);
+});
+
+
+// autoscaling.scale('eu-central-1', [{ AutoScalingGroupName: 'JacquardPreProd-JacquardDirectEuWest1AutoScalingGroup-193UO71ZHICH2', DesiredCapacity: 234 }], '10%', function(err, res) {
+// 	console.log(err);
+// 	console.log(res);
+// });
+
+// autoscaling.scale('eu-central-1', { AutoScalingGroupName: 'JacquardPreProd-JacquardDirectEuWest1AutoScalingGroup-193UO71ZHICH2', DesiredCapacity: 234 }, '10%', function(err, res) {
+// 	console.log(err);
+// 	console.log(res);
+// });
