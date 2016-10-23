bc146466e6c7b7961b1110f236e274d5e2a1d874
David Oliveira <david@Davids-MacBook.local>
2016-10-23 22:44:20 +0100

    WIP. AWS resource selector query parsing

--- test/aws/00_main.js
+++ test/aws/00_main.js
@@ -2,4 +2,11 @@
 
 var aws = require('../../lib/aws');
 
-console.log(aws.$('elb#jqd-Prod'))
+console.log(JSON.stringify(aws.$('elb')))
+console.log(JSON.stringify(aws.$('#jqd-Prod')))
+console.log(JSON.stringify(aws.$('.ble.bli')))
+try { console.log(aws.$('#jqd-Prod#jqd-Wtv')) } catch(ex){ console.log(ex); }
+console.log(JSON.stringify(aws.$('elb#jqd-Prod.bla.bli')))
+console.log(JSON.stringify(aws.$('elb.bla.bli')))
+console.log(JSON.stringify(aws.$('elb#jqd-Prod.bla.bli .box.stuff')))
+console.log(JSON.stringify(aws.$('elb#jqd-Prod.bla.bli > .box.stuff')))
