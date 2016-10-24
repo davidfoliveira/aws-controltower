3170ecdbd5291febb1f3f1ed824a23b6d2c755af
David Oliveira <david.oliveira@skyscanner.net>
2016-10-24 10:38:48 +0100

    Small test changes and loggin

--- test/aws/02_ec2/02_autoscaling/02_scale.js
+++ test/aws/02_ec2/02_autoscaling/02_scale.js
@@ -1,18 +1,43 @@
-var
-    autoscaling = require('../../../../lib/aws/ec2/autoscaling');
+ var
+     async = require('async'),
+     autoscaling = require('../../../lib/aws/autoscaling');
 
-autoscaling.scale('eu-west-1', ['JacquardPreProd-JacquardDirectEuWest1AutoScalingGroup-193UO71ZHICH2'], '+20%', {dryRun: true}, function(err, res) {
-	console.log(err);
-	console.log(res);
-});
-
-
-// autoscaling.scale('eu-central-1', [{ AutoScalingGroupName: 'JacquardPreProd-JacquardDirectEuWest1AutoScalingGroup-193UO71ZHICH2', DesiredCapacity: 234 }], '10%', function(err, res) {
-// 	console.log(err);
-// 	console.log(res);
-// });
-
-// autoscaling.scale('eu-central-1', { AutoScalingGroupName: 'JacquardPreProd-JacquardDirectEuWest1AutoScalingGroup-193UO71ZHICH2', DesiredCapacity: 234 }, '10%', function(err, res) {
-// 	console.log(err);
-// 	console.log(res);
-// });
+async.series(
+    [
+        function(next){
+            autoscaling.scale('eu-west-1', ['JacquardPreProd-JacquardDirectEuWest1AutoScalingGroup-193UO71ZHICH2'], '+20%', {dryRun: true}, function(err, res) {
+                if ( err ) {
+                    console.log("Error: ", err);
+                    return next(err,false);
+                }
+                console.log(res);
+                return next(null,true);
+            });
+        },
+        function(next){
+            autoscaling.scale('eu-central-1', [{ AutoScalingGroupName: 'JacquardPreProd-JacquardDirectEuWest1AutoScalingGroup-193UO71ZHICH2', DesiredCapacity: 234 }], '-10%', {dryRun: true}, function(err, res) {
+                if ( err ) {
+                    console.log("Error: ", err);
+                    return next(err,false);
+                }
+                console.log(res);
+                return next(null,true);
+            });
+        },
+        function(next){
+            autoscaling.scale('eu-central-1', 'JacquardPreProd-JacquardDirectEuWest1AutoScalingGroup-193UO71ZHICH2', '+10%', {dryRun: true}, function(err, res) {
+                if ( err ) {
+                    console.log("Error: ", err);
+                    return next(err,false);
+                }
+                console.log(res);
+                return next(null,true);
+            });
+        }
+    ],
+    function(err, res){
+        if ( err )
+            return process.exit(-1);
+        return process.exit(0);
+    }
+);
