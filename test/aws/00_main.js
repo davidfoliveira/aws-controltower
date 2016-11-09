cb13f4841f60a1eb72e6a5a655830ec68b333e39
David Oliveira <david.oliveira@skyscanner.net>
2016-11-09 16:31:54 +0000

    Support for tags and ELB filtering

--- test/aws/00_main.js
+++ test/aws/00_main.js
@@ -2,21 +2,25 @@
 
 var aws = require('../../lib/aws');
 
-console.log(JSON.stringify(aws.$('elb@eu-central-1')))
-console.log(JSON.stringify(aws.$('#jqd-Prod@eu-west-1')))
-console.log(JSON.stringify(aws.$('.ble.bli')))
-try { console.log(aws.$('#jqd-Prod#jqd-Wtv')) } catch(ex){ console.log(ex); }
-console.log(JSON.stringify(aws.$('elb#jqd-Prod.bla.bli')))
-console.log(JSON.stringify(aws.$('elb.bla.bli')))
-console.log(JSON.stringify(aws.$('elb#jqd-Prod.bla.bli .box.stuff')))
-console.log(JSON.stringify(aws.$('elb#jqd-Prod.bla.bli > .box.stuff')))
+// console.log("1: ", JSON.stringify(aws.$('elb@eu-central-1'), 0, 4))
+// console.log("2: ", JSON.stringify(aws.$('#jqd-Prod@eu-west-1'), 0, 4))
+// console.log("3: ", JSON.stringify(aws.$('[TagName1][TagName2=TagValue2]'), 0, 4))
+// console.log("4: ", JSON.stringify(aws.$('[TagName1=TagValue1][TagName2=TagValue2]'), 0, 4))
+// console.log("5: ", JSON.stringify(aws.$('[TagName1^=TagValue1][TagName2*=TagValue2]'), 0, 4))
+// console.log("6: ", JSON.stringify(aws.$('[TagName1|="TagValue1 something"][TagName2$=TagValue2]'), 0, 4))
+
+// try { console.log("7: ", aws.$('#jqd-Prod#jqd-Wtf'), 0, 4) } catch(ex){ console.log(ex); }
+// console.log("8: ", JSON.stringify(aws.$('elb#jqd-Prod[TagName1=TagValue1][TagName2=TagValue2]'), 0, 4))
+// console.log("9: ", JSON.stringify(aws.$('elb[TagName1=TagValue1][TagName2=TagValue2]'), 0, 4))
+// console.log("10: ", JSON.stringify(aws.$('elb#jqd-Prod[TagName1=TagValue1][TagName2=TagValue2] [TagName1=TagValue1][TagName2=TagValue2]'), 0, 4))
+// console.log("11: ", JSON.stringify(aws.$('elb#jqd-Prod[TagName1=TagValue1][TagName2=TagValue2] > [TagName1=TagValue1][TagName2=TagValue2]'), 0, 4))
 
 
-aws.getResources(aws.$("elb#Test@eu-west-1"), function(err, res){
-	if ( err ) {
-		console.log("Error getting all elbs");
-		return process.exit(-1);
-	}
-	console.log(res);
-	return process.exit(0);
+aws.getResources(aws.$('elb@eu-west-1#Test[TagName1$=1][TagName2=TagValue2]'), function(err, res){
+    if ( err ) {
+        console.log("Error getting all elbs");
+        return process.exit(-1);
+    }
+    console.log(res);
+    return process.exit(0);
 });
